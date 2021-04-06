e3a7fc3c7b5042207ca4a22603051ce8de88b84f
Hong Nguyen <hongnguyen020499@gmail.com>
2021-04-06 09:51:45 +0700

    update

---
+++ 4IN1_BackEnd/src/main/java/com/example/demo/Service/impl/UserServicelmpl.java
@@ -0,0 +1,35 @@
+package com.example.demo.Service.impl;
+
+import com.example.demo.Entity.User;
+import com.example.demo.Repository.UserRepository;
+import com.example.demo.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServicelmpl implements UserService {
+    @Autowired private UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUser() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @Override
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<User> findUserById(Long id) {
+        return userRepository.findById(id);
+    }
+}
