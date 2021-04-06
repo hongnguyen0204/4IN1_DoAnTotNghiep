ba4470ae67ed79206e1f6083f3221050617c5790
haunguyen140299 <haunguyen140299@gmail.com>
2021-04-06 15:08:51 +0700

    a

--- 4IN1_BackEnd/src/main/java/com/example/main/Controller/SuKienController.java
+++ 4IN1_BackEnd/src/main/java/com/example/main/Controller/SuKienController.java
@@ -1,5 +1,23 @@
 package com.example.main.Controller;
 
+
+import com.example.main.Entity.SuKien;
+import com.example.main.Repository.SuKienRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin (origins = "http://localhost:4200")
 public class SuKienController {
-   
+   @Autowired
+    private SuKienRepository suKienRepository;
+
+   @GetMapping("/sukien")
+    public List<SuKien> getSuKiens() {
+    return suKienRepository.findAll();
+   }
 }
