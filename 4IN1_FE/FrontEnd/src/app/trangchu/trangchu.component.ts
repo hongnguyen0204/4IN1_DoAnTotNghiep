97ea58de36ccc443ff75a670116783124ef41b8b
haunguyen140299 <haunguyen140299@gmail.com>
2021-04-08 10:59:04 +0700

    a

--- 4IN1_FE/FrontEnd/src/app/trangchu/trangchu.component.ts
+++ 4IN1_FE/FrontEnd/src/app/trangchu/trangchu.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Sukien} from '../Model/sukien';
+import {SukienService} from '../Service/sukien.service';
 
 @Component({
   selector: 'app-trangchu',
   templateUrl: './trangchu.component.html',
-  styleUrls: ['./trangchu.component.scss']
+  styleUrls: ['./trangchu.component.scss'],
+  providers: [SukienService]
 })
 export class TrangchuComponent implements OnInit {
+  // @ts-ignore
+  sukiens: Sukien[];
+  // @ts-ignore
+  m :number; //Biến lấy tháng hiện tại
 
-  constructor() { }
+  constructor(private sukienService: SukienService) {
+  }
 
   ngOnInit(): void {
+    this.reloadData();
+    var d = new Date();
+    this.m = d.getMonth() + 1;
   }
+
+  reloadData() {
+    this.sukienService.findAll().subscribe(data => {
+      this.sukiens = data;
+    })
+  }
+
+
 
 }
