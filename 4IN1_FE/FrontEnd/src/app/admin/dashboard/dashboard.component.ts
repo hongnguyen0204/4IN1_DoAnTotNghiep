10780075bb245e81b5e77242744375d662efba5a
Hong Nguyen <hongnguyen020499@gmail.com>
2021-04-12 22:15:01 +0700

    dashboard

--- 4IN1_FE/FrontEnd/src/app/admin/dashboard/dashboard.component.ts
+++ 4IN1_FE/FrontEnd/src/app/admin/dashboard/dashboard.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import {DashboardService} from '../../Service/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  providers:[DashboardComponent]
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  // @ts-ignore
+  dtOptions: { pagingType: string };
+  // @ts-ignore
+  skdtc:number;
+  // @ts-ignore
+  tsk:number;
+  // @ts-ignore
+  tskdd:number;
+
+  constructor(private dashboardService:DashboardService) { }
 
   ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers'
+    };
+    this.dashboardService.SKDTC().subscribe(data=>{
+      this.skdtc=data;
+    });
+    this.dashboardService.TSK().subscribe(data=>{
+      this.tsk=data;
+    });
+    this.dashboardService.TSKDD().subscribe(data=>{
+      this.tskdd.
+    })
   }
 
 }
