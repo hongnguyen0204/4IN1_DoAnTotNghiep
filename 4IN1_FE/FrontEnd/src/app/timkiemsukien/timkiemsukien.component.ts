c110dc8aa8a25d425714cc247c28d363495aefc7
haunguyen140299 <haunguyen140299@gmail.com>
2021-04-12 11:39:51 +0700

    a

--- 4IN1_FE/FrontEnd/src/app/timkiemsukien/timkiemsukien.component.ts
+++ 4IN1_FE/FrontEnd/src/app/timkiemsukien/timkiemsukien.component.ts
@@ -1,5 +1,7 @@
+import {Component, OnInit} from '@angular/core';
+import {Sukien} from '../Model/sukien';
 import {SukienService} from '../Service/sukien.service';
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-timkiemsukien',
