70ac6ab22b537b418514da3dfee155255cd749d9
Hong Nguyen <hongnguyen020499@gmail.com>
2021-04-14 09:04:56 +0700

    a

--- Back_End/src/main/java/com/bezkoder/springjwt/repository/DangKiLamCTVRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bezkoder.springjwt.repository;
-
-import com.bezkoder.springjwt.models.DangKiLamCTV;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DangKiLamCTVRepository extends JpaRepository <DangKiLamCTV,Integer>{
-}
