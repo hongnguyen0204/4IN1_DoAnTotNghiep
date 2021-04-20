535884b2cd80b57b1f2eca95fe0d0ea69c498cf6
Hong Nguyen <hongnguyen020499@gmail.com>
2021-04-20 08:07:35 +0700

    update

--- 4IN1_FE/FrontEnd/src/app/_services/customer.service.ts
+++ 4IN1_FE/FrontEnd/src/app/_services/customer.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router} from '@angular/router';
-import {MatSnackBar} from '@angular/material/snack-bar';
 import {TokenStorageService} from './token-storage.service';
 
 @Injectable({
@@ -10,23 +9,16 @@ import {TokenStorageService} from './token-storage.service';
 export class CustomerAuthService implements CanActivate{
 
   constructor(private tokenStorageService: TokenStorageService,
-              private router: Router,
-              private matSnackBar: MatSnackBar) { }
+              private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot): boolean {
     const token = this.tokenStorageService.getToken();
 
     if (token == null) {
       this.router.navigateByUrl('/dangnhap');
-      this.matSnackBar.open('Bạn cần đăng nhập để thực hiện tác vụ!', 'Ok', {
-        duration: 4000,
-      });
       return false;
     } else if (!this.isRole()) {
       this.router.navigateByUrl('/dangnhap');
-      this.matSnackBar.open('Bạn không có quyền truy cập trang này!', 'Ok', {
-        duration: 4000,
-      });
       return false;
     }  else {
       return true;
