29f20c6d03340030f339ff78c1a1ecb430a1c38a
haunguyen140299 <haunguyen140299@gmail.com>
2021-04-21 18:41:16 +0700

    hau sua chuc nang mo tab moi khong can dang nhap

--- 4IN1_FE/FrontEnd/src/app/_services/token-storage.service.ts
+++ 4IN1_FE/FrontEnd/src/app/_services/token-storage.service.ts
@@ -11,27 +11,27 @@ export class TokenStorageService {
   constructor() { }
 
   signOut(): void {
-    window.sessionStorage.clear();
+    window.localStorage.clear();
   }
 
   public saveToken(token: string): void {
-    window.sessionStorage.removeItem(TOKEN_KEY);
-    window.sessionStorage.setItem(TOKEN_KEY, token);
+    window.localStorage .removeItem(TOKEN_KEY);
+    window.localStorage .setItem(TOKEN_KEY, token);
   }
 
   public getToken(): string {
     // @ts-ignore
-    return sessionStorage.getItem(TOKEN_KEY);
+    return localStorage .getItem(TOKEN_KEY);
   }
 
   // @ts-ignore
   public saveUser(user): void {
-    window.sessionStorage.removeItem(USER_KEY);
-    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+    window.localStorage .removeItem(USER_KEY);
+    window.localStorage .setItem(USER_KEY, JSON.stringify(user));
   }
 
   public getUser(): any {
     // @ts-ignore
-    return JSON.parse(sessionStorage.getItem(USER_KEY));
+    return JSON.parse(localStorage .getItem(USER_KEY));
   }
 }
