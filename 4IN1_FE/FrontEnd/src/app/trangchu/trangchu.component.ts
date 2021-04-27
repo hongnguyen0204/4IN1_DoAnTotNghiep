3a55387f4622c8daf21450d9f9ddc65c7d036cb6
haunguyen140299 <haunguyen140299@gmail.com>
2021-04-27 16:34:13 +0700

    sửa sự kiện hot

--- 4IN1_FE/FrontEnd/src/app/trangchu/trangchu.component.ts
+++ 4IN1_FE/FrontEnd/src/app/trangchu/trangchu.component.ts
@@ -45,8 +45,6 @@ export class TrangchuComponent implements OnInit {
       this.sukien=data;
     })
   }
-
-<<<<<<< HEAD
   datahot(){
     // @ts-ignore
     this.sukienService.findByhot().subscribe(data=>{
@@ -61,10 +59,10 @@ export class TrangchuComponent implements OnInit {
         var minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
         var seconds = Math.floor((distance % (1000*60)) / 1000);
         this.demo = days + "d " + hours + "h " +minutes +"m " +seconds + "s ";
-        $("#days").html(days + "<span style='font-size: 30px; margin-top: -45px'>Days</span>");
-        $("#hours").html(hours + "<span style='font-size: 30px; margin-top: -45px'>Hours</span>");
-        $("#minutes").html(minutes + "<span style='font-size: 30px; margin-top: -45px'>Minutes</span>");
-        $("#seconds").html(seconds + "<span style='font-size: 30px; margin-top: -45px'>Seconds</span>");
+        $("#days").html(days + "<span style='font-size: 30px; margin-top: -30px'>Days</span>");
+        $("#hours").html(hours + "<span style='font-size: 30px; margin-top: -30px'>Hours</span>");
+        $("#minutes").html(minutes + "<span style='font-size: 30px; margin-top: -30px'>Minutes</span>");
+        $("#seconds").html(seconds + "<span style='font-size: 30px; margin-top: -30px'>Seconds</span>");
       })
     });
 
@@ -73,13 +71,5 @@ export class TrangchuComponent implements OnInit {
   guilienlac(){
     this.sukienService.sendmessage(this.message).subscribe();
   }
-
-
-  // reloadData() {
-  //   this.sukienService.findAll().subscribe(data => {
-  //     this.sukiens = data;
-  //   })
-=======
->>>>>>> 6e6867a9fca57d1227af675fe713c9506cea207e
   }
 
